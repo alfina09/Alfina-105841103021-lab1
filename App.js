a17079801fb40c0574d2bca550efd51b1545f481
alfina09 <105841103021@student.unismuh.ac.id>
2024-06-03 01:47:35 -0700

    useFont

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet,Text,View } from 'react-native'
 import React from 'react'
-import { useFonts } from 'expo-font'
+import{useFonts} from 'expo-font'
+
 const App = () => {
-  const [fontsLoaded, fontError] = useFonts({
+  const [fontsLoaded,fontError] = useFonts({
     'Metro-Bold': require('./assets/fonts/Metropolis-Bold.otf'),
     'Metro-Medium': require('./assets/fonts/Metropolis-Medium.otf'),
     'Metro-SemiBold': require('./assets/fonts/Metropolis-SemiBold.otf'),
     'Metro-Black': require('./assets/fonts/Metropolis-Black.otf'),
   });
-  if (!fontsLoaded) return <View>
-    <Text>Font tidak ditemukan !</Text>
-  </View>
-  return (
-    <View >
-      <Text >Font Biasa</Text>
-      <Text style={{ fontFamily: 'Metro-Bold', fontSize: 30 }}>Metro Bold</Text>
-      <Text style={{ fontFamily: 'Metro-Medium', fontSize: 30 }}>Metro Medium</Text>
-      <Text style={{ fontFamily: 'Metro-SemiBold', fontSize: 30 }}>Metro SemiBold</Text>
-      <Text style={{ fontFamily: 'Metro-Black', fontSize: 30 }}>Metro Black</Text>
-    </View>
-  )
+
+  if (!fontsLoaded) {
+    return (
+        <View>
+            <Text>Font tidak ditemukan!</Text>
+        </View>
+    );
 }
+
+  return (
+    <View style={{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+    }}>
+
+      <Text style ={{fontSize:30}}>Font Biasa</Text>
+      <Text style ={{fontFamily:'Metro-Bold', fontSize:30}}>Metro Bold</Text>
+      <Text style ={{fontFamily:'Metro-Medium', fontSize:30}}>Metro Medium</Text>
+      <Text style ={{fontFamily:'Metro-SemiBold', fontSize:30}}>Metro SemiBold</Text>
+      <Text style ={{fontFamily:'Metro-Black', fontSize:30}}>Metro Black</Text>
+    </View>
+  );
+}
+
 export default App
